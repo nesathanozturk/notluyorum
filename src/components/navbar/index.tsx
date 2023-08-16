063d1e325db40fe236b2a7af295b4681cc9c813e
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-08-16 14:53:16 +0300

    feat: add react-icons

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,8 +1,9 @@
+import { BiSun } from "react-icons/bi";
 import Image from "next/image";
 
 const Navbar = () => {
   return (
-    <div className="contaier mx-auto navbar py-6 px-16 justify-between border-b-[0.05rem] border-[#333444]">
+    <div className="contaier mx-auto navbar py-6 px-5 md:px-16 justify-between border-b-[0.05rem] border-[#333444]">
       <div className="avatar space-x-4">
         <div className="w-10 rounded-full ring ring-accent ring-offset-base-100 ring-offset-2">
           <Image
@@ -14,9 +15,14 @@ const Navbar = () => {
         </div>
         <span className="font-bold text-gray-200 text-sm">Neşathan Öztürk</span>
       </div>
-      <button className="btn btn-accent capitalize text-black font-medium">
-        Add New Note
-      </button>
+      <div className="flex space-x-3">
+        <span className="cursor-pointer">
+          <BiSun size={25} />
+        </span>
+        <button className="btn btn-accent capitalize text-black font-medium text-xs md:text-sm">
+          Add New Note
+        </button>
+      </div>
     </div>
   );
 };
