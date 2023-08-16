ccee3f0dcbdfb29afd392bffd7de10cd967bd5aa
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-08-16 21:59:11 +0300

    feat: add dark mode classnames

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -20,9 +20,9 @@ const Navbar = () => {
 
   return (
     <>
-      <div className="contaier mx-auto navbar py-6 px-5 md:px-16 justify-between border-b-[0.05rem] border-[#393939]">
+      <div className="bg-[#EAEAEA] dark:bg-[#303030] contaier mx-auto navbar py-6 px-5 md:px-16 justify-between border-b-[0.05rem] border-[#393939]">
         <div className="avatar space-x-4">
-          <div className="w-10 rounded-full ring ring-accent ring-offset-base-100 ring-offset-2 cursor-pointer">
+          <div className="w-10 rounded-full ring ring-black dark:ring-accent ring-offset-base-100 ring-offset-2 cursor-pointer">
             <Image
               src="https://avatars.githubusercontent.com/u/107864503?v=4"
               alt="Avatar"
@@ -30,7 +30,7 @@ const Navbar = () => {
               height={40}
             />
           </div>
-          <span className="font-bold text-gray-200 text-xs sm:text-sm">
+          <span className="font-bold text-gray-600 dark:text-gray-200 text-xs sm:text-sm">
             Neşathan Öztürk
           </span>
         </div>
@@ -42,7 +42,7 @@ const Navbar = () => {
           />
           <button
             onClick={handleOpen}
-            className="btn-accent py-2 px-4 rounded-lg capitalize text-[#1f2d2b] font-medium text-xs md:text-sm transition-colors"
+            className="bg-[#184D47] dark:btn-accent py-2 px-4 rounded-lg capitalize text-[#E8EDED] dark:text-[#1f2d2b] font-medium text-xs md:text-sm transition-colors"
           >
             Not Oluştur
           </button>
