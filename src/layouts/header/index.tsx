f0df43934ff2b2996e2f502c672a99d00fd4351a
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-08-16 13:52:44 +0300

    style: change title color

--- src/layouts/header/index.tsx
+++ src/layouts/header/index.tsx
@@ -2,7 +2,7 @@ const Header = () => {
   return (
     <header className="navbar py-4">
       <nav className="container mx-auto px-6 justify-between flex-wrap">
-        <h1 className="normal-case text-2xl md:text-3xl font-bold italic text-gray-100 tracking-wide">
+        <h1 className="text-2xl md:text-3xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
           Notluyorum
         </h1>
         <button className="btn btn-active btn-primary text-xs md:text-sm tracking-widest">
