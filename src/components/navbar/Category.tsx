3650d4aa6a2a8a7d63b9e08c172a6787941115f6
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-08-16 16:13:17 +0300

    feat: create category

---
+++ src/components/navbar/Category.tsx
@@ -0,0 +1,24 @@
+import { categories } from "@/utils/data";
+
+const Category = () => {
+  const renderedCategories = categories.map((category) => (
+    <div
+      key={category.id}
+      className="flex justify-between items-center p-4 px-6 border-b-[0.05rem] border-[#393939] cursor-pointer hover:bg-[#393939] hover:bg-opacity-50 transition-all"
+    >
+      <div className="flex items-center space-x-4">
+        <div className={`w-5 h-5 bg-[${category.color}] rounded-full`}></div>
+        <span className="font-bold">{category.category}</span>
+      </div>
+      <div
+        className={`w-10 py-[0.3em] bg-[${category.lengthColor}] rounded-xl text-center text-white font-semibold text-sm`}
+      >
+        {category.length}
+      </div>
+    </div>
+  ));
+
+  return renderedCategories;
+};
+
+export default Category;
---
+++ src/utils/data.ts
@@ -0,0 +1,44 @@
+export const categories = [
+  {
+    id: 1,
+    color: "#AE54A2",
+    category: "All Notes",
+    length: 12,
+    lengthColor: "#AF54A3",
+  },
+  {
+    id: 2,
+    color: "#A35E11",
+    category: "Wishlist",
+    length: 0o2,
+    lengthColor: "#A45F12",
+  },
+  {
+    id: 3,
+    color: "#4757A5",
+    category: "Assigment",
+    length: 0o3,
+    lengthColor: "#4758A6",
+  },
+  {
+    id: 4,
+    color: "#349B90",
+    category: "Projects",
+    length: 0o1,
+    lengthColor: "#349B90",
+  },
+  {
+    id: 5,
+    color: "#B64963",
+    category: "Work",
+    length: 0o4,
+    lengthColor: "#B74963",
+  },
+  {
+    id: 6,
+    color: "#B58829",
+    category: "Study",
+    length: 0o2,
+    lengthColor: "#B6892A",
+  },
+];
