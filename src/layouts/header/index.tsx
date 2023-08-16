2276d086815753a9d39107a1892ec9101e3b73c1
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-08-16 14:03:33 +0300

    style: edit hero and header

--- src/layouts/header/index.tsx
+++ src/layouts/header/index.tsx
@@ -1,12 +1,12 @@
 const Header = () => {
   return (
-    <header className="navbar py-4">
+    <header className="navbar pt-6">
       <nav className="container mx-auto px-6 justify-between flex-wrap">
-        <h1 className="text-2xl md:text-3xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
+        <h1 className="text-3xl md:text-4xl font-extrabold text-transparent bg-gradient-to-r from-blue-500 to-purple-600 bg-clip-text">
           Notluyorum
         </h1>
-        <button className="btn btn-active btn-primary text-xs md:text-sm tracking-widest">
-          Çıkış yap
+        <button className="btn btn-primary tracking-widest text-xs md:text-sm">
+          Çıkış Yap
         </button>
       </nav>
     </header>
--- src/layouts/hero/index.tsx
+++ src/layouts/hero/index.tsx
@@ -3,10 +3,10 @@ const Hero = () => {
     <section className="hero min-h-screen">
       <div className="hero-content text-center">
         <div className="max-w-4xl">
-          <h1 className="text-3xl md:text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
+          <h1 className="text-3xl md:text-5xl font-extrabold text-transparent bg-gradient-to-r from-blue-500 to-purple-600 bg-clip-text">
             Notluyorum&apos;a Hoş Geldiniz!
           </h1>
-          <p className="py-6 text-xl md:text-2xl font-semibold tracking-wide">
+          <p className="py-6 text-xl md:text-2xl text-gray-300 font-semibold tracking-wide">
             Her an aklınıza gelen fikirleri, yapılacakları veya ilham veren
             notları kaydetmek hiç bu kadar kolay olmamıştı. Notluyorum ile
             hayatınızı düzenlemek ve unutmamak için güçlü bir araca sahip
@@ -15,7 +15,7 @@ const Hero = () => {
             unutmak istemediğiniz her şeyi burada bir araya getirin ve en iyi
             şekilde organize edin.
           </p>
-          <button className="btn btn-primary">Başlayın!</button>
+          <button className="btn btn-primary tracking-widest">Başlayın!</button>
         </div>
       </div>
     </section>
