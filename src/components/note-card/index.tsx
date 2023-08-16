121f1d890547c360abb657a1c3b6ac256a9e6281
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-08-16 17:59:45 +0300

    style: edit size

--- src/components/note-card/index.tsx
+++ src/components/note-card/index.tsx
@@ -4,18 +4,18 @@ const NoteCard = () => {
   const renderedNoteCard = notes.map((note) => (
     <div
       key={note.id}
-      className={`bg-[${note.cardColor}] py-5 px-6 mb-6 md:max-w-[25rem] rounded-3xl cursor-pointer`}
+      className={`bg-[${note.cardColor}] py-5 px-6 mb-6 md:max-w-[25rem] rounded-3xl cursor-pointer hover:bg-opacity-90 transition-colors`}
     >
       <div className="flex justify-between items-center flex-wrap gap-2 mb-3">
         <h1 className="font-bold text-white">{note.title}</h1>
         <span
-          className={`px-5 py-[0.9em] bg-white rounded-xl text-center text-[${note.categoryTextColor}] font-semibold text-xs uppercase tracking-widest`}
+          className={`px-3 py-[0.7em] sm:px-5 sm:py-[0.9em] bg-white rounded-xl text-center text-[${note.categoryTextColor}] font-semibold text-xs uppercase tracking-widest`}
         >
           {note.category}
         </span>
       </div>
       <p className="text-white text-xs leading-6">{note.description}</p>
-      <div className="flex justify-between items-center flex-wrap gap-2 text-[#D9DEF2] font-bold uppercase mt-7 text-sm">
+      <div className="flex justify-between items-center flex-wrap gap-2 text-[#D9DEF2] font-bold uppercase mt-7 text-xs sm:text-sm">
         <span>{note.createdTime}</span>
         <span>{note.createdDate}</span>
       </div>
