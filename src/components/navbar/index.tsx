74f09fb598491c264e766cf776912794e79d43f5
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-08-16 18:01:26 +0300

    style: add transiiton

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <div className="contaier mx-auto navbar py-6 px-5 md:px-16 justify-between border-b-[0.05rem] border-[#393939]">
       <div className="avatar space-x-4">
-        <div className="w-10 rounded-full ring ring-accent ring-offset-base-100 ring-offset-2">
+        <div className="w-10 rounded-full ring ring-accent ring-offset-base-100 ring-offset-2 cursor-pointer">
           <Image
             src="https://avatars.githubusercontent.com/u/107864503?v=4"
             alt="Avatar"
@@ -21,7 +21,7 @@ const Navbar = () => {
         <span className="cursor-pointer">
           <BiSun size={25} />
         </span>
-        <button className="btn-accent py-2 px-4 rounded-lg capitalize text-[#1f2d2b] font-medium text-xs md:text-sm">
+        <button className="btn-accent py-2 px-4 rounded-lg capitalize text-[#1f2d2b] font-medium text-xs md:text-sm transition-colors">
           Add New Note
         </button>
       </div>
