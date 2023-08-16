1c2b4456dec6cae8e66de29c20b67759fab7f076
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-08-16 18:03:35 +0300

    style: add text-center to h1

--- src/components/note-card/index.tsx
+++ src/components/note-card/index.tsx
@@ -7,7 +7,9 @@ const NoteCard = () => {
       className={`bg-[${note.cardColor}] py-5 px-6 mb-6 md:max-w-[25rem] rounded-3xl cursor-pointer hover:bg-opacity-90 transition-colors`}
     >
       <div className="flex justify-between items-center flex-wrap gap-2 mb-3">
-        <h1 className="font-bold text-white">{note.title}</h1>
+        <h1 className="text-center sm:text-left font-bold text-white">
+          {note.title}
+        </h1>
         <span
           className={`px-3 py-[0.7em] sm:px-5 sm:py-[0.9em] bg-white rounded-xl text-center text-[${note.categoryTextColor}] font-semibold text-xs uppercase tracking-widest`}
         >
