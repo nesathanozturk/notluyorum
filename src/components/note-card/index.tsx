b086e17e9857765e92dda1c9d8ff30d257160f53
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-08-17 21:25:22 +0300

    style: change bg color

--- src/components/note-card/index.tsx
+++ src/components/note-card/index.tsx
@@ -10,11 +10,11 @@ const NoteCard = () => {
   const renderedNoteCard = notes?.map((note) => (
     <div
       key={note.title}
-      className="bg-green-600 dark:bg-green-800 py-5 px-6 mb-6 md:max-w-[26.3rem] rounded-3xl cursor-pointer hover:bg-opacity-90 transition-colors text-[#343334] dark:text-white relative"
+      className="bg-teal-500 dark:bg-teal-600 py-5 px-6 mb-6 md:max-w-[26.3rem] rounded-3xl cursor-pointer hover:bg-opacity-90 transition-colors text-[#343334] dark:text-white relative"
     >
       <div className="flex justify-between items-center flex-wrap gap-2 mb-3">
         <h1 className="text-center sm:text-left font-bold">{note?.title}</h1>
-        <span className="px-3 py-[0.7em] sm:px-5 sm:py-[0.9em] text-white dark:bg-white rounded-xl text-center dark:text-green-600 font-semibold text-xs uppercase tracking-widest">
+        <span className="px-3 py-[0.7em] sm:px-5 sm:py-[0.9em] bg-teal-700 text-white dark:bg-white rounded-xl text-center dark:text-green-600 font-semibold text-xs uppercase tracking-widest">
           {note?.category}
         </span>
       </div>
