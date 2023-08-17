9907a42c3fc628eb792c78a8ee96f9e1a8fbc215
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-08-17 21:06:17 +0300

    feat: add padding

--- src/layouts/header/index.tsx
+++ src/layouts/header/index.tsx
@@ -16,7 +16,7 @@ const Header = () => {
 
   return (
     <header className="navbar pt-4">
-      <nav className="container mx-auto md:px-6 justify-between flex-wrap">
+      <nav className="container mx-auto px-3 md:px-6 justify-between flex-wrap">
         <h1
           title="Notluyorum"
           className="text-3xl md:text-4xl font-extrabold text-transparent bg-gradient-to-r from-blue-500 to-purple-600 bg-clip-text cursor-pointer"
