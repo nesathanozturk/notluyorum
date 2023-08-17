63ccee4cbea69b63c2546ded4e5630b059430613
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-08-17 21:05:47 +0300

    feat: add color to categories

--- src/components/category/index.tsx
+++ src/components/category/index.tsx
@@ -7,13 +7,15 @@ const Category = () => {
       className="flex justify-between items-center flex-wrap p-4 px-6 border-b-[0.05rem] border-[#e4e4e4] dark:border-[#505050] cursor-pointer hover:bg-[#393939] hover:bg-opacity-10 dark:hover:bg-opacity-50 transition-all"
     >
       <div className="flex items-center space-x-4">
-        <div className={`w-5 h-5 bg-${category.color} rounded-full`}></div>
+        <div
+          className={`w-5 h-5 ${category?.color} ${category?.darkColor} rounded-full`}
+        ></div>
         <span className="font-bold text-[#818181] dark:text-gray-400">
           {category.category}
         </span>
       </div>
       <span
-        className={`w-10 py-[0.3em] bg-${category.lengthColor} dark:bg-${category.darkLengthColor} rounded-xl text-center text-white font-semibold text-sm`}
+        className={`w-10 py-[0.3em] ${category?.lengthColor} ${category?.darkLengthColor} rounded-xl text-center text-white font-semibold text-sm`}
       >
         {category.length}
       </span>
