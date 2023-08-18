6bad27b0cca5d96f47e99c403e1fca21d3d9d274
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-08-18 14:33:01 +0300

    feat: add edit button

--- src/components/note-card/index.tsx
+++ src/components/note-card/index.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { TiDelete } from "react-icons/ti";
+import { AiFillEdit } from "react-icons/ai";
 
 import { useNoteContext } from "@/context/NoteContext";
 
@@ -12,15 +13,21 @@ const NoteCard = () => {
   const renderedNoteCard = notes?.map((note) => (
     <div
       key={note?.id}
-      className="bg-teal-500 dark:bg-teal-600 py-5 px-6 mb-6 md:max-w-[26.3rem] rounded-3xl cursor-pointer hover:bg-opacity-90 transition-colors text-[#343334] dark:text-white relative"
+      className="bg-teal-500 dark:bg-teal-600 py-5 px-6 mb-6 md:max-w-[26.3rem] rounded-3xl cursor-pointer hover:bg-opacity-80 hover:dark:bg-opacity-70 transition-colors text-[#343334] dark:text-white"
     >
-      <button
-        className="absolute right-3 top-1 text-teal-900 dark:text-white"
-        onClick={() => handleDeleteNote?.(note?.id)}
-      >
-        <TiDelete size={30} />
-      </button>
-      <div className="flex justify-between items-center flex-wrap gap-2 mt-6 mb-2">
+      <div className="flex justify-end items-center">
+        <button className="text-teal-900 dark:text-white" title="Düzenle">
+          <AiFillEdit size={30} />
+        </button>
+        <button
+          className="text-teal-900 dark:text-white"
+          onClick={() => handleDeleteNote?.(note?.id)}
+          title="Sil"
+        >
+          <TiDelete size={30} />
+        </button>
+      </div>
+      <div className="flex justify-between items-center flex-wrap gap-2 my-4">
         <h1 className="text-center sm:text-left font-bold">{note?.title}</h1>
         <span className="px-3 py-[0.7em] sm:px-5 sm:py-[0.9em] bg-teal-700 text-white dark:bg-white rounded-xl text-center dark:text-green-600 font-semibold text-xs uppercase tracking-widest">
           {note?.category}
