2c414e2eecb1eee0a0cc4774b4c763c8de13c3cc
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-08-18 13:52:09 +0300

    style: edit remvoe button

--- src/components/note-card/index.tsx
+++ src/components/note-card/index.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { TiDelete } from "react-icons/ti";
+
 import { useNoteContext } from "@/context/NoteContext";
 
 import { INote } from "@/types";
@@ -12,19 +14,19 @@ const NoteCard = () => {
       key={note?.id}
       className="bg-teal-500 dark:bg-teal-600 py-5 px-6 mb-6 md:max-w-[26.3rem] rounded-3xl cursor-pointer hover:bg-opacity-90 transition-colors text-[#343334] dark:text-white relative"
     >
-      <div className="flex justify-between items-center flex-wrap gap-2 mb-3">
+      <button
+        className="absolute right-3 top-1 text-teal-900 dark:text-white"
+        onClick={() => handleDeleteNote?.(note?.id)}
+      >
+        <TiDelete size={30} />
+      </button>
+      <div className="flex justify-between items-center flex-wrap gap-2 mt-6 mb-2">
         <h1 className="text-center sm:text-left font-bold">{note?.title}</h1>
         <span className="px-3 py-[0.7em] sm:px-5 sm:py-[0.9em] bg-teal-700 text-white dark:bg-white rounded-xl text-center dark:text-green-600 font-semibold text-xs uppercase tracking-widest">
           {note?.category}
         </span>
       </div>
       <p className="text-xs leading-6">{note?.description}</p>
-      <button
-        className="absolute btn btn-sm btn-error left-3 bottom-3 text-white bg-red-600 border-0"
-        onClick={() => handleDeleteNote?.(note?.id)}
-      >
-        Kaldır
-      </button>
     </div>
   ));
 
