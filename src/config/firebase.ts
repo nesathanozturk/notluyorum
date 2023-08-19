5d0dea07c97d4f9dcdf1153186ae791de060be51
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-08-19 16:43:34 +0300

    feat: create env

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -5,13 +5,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDKn-Jy1aT6_BogXC6visewlVWntZmzObo",
-  authDomain: "notluyorum-76d68.firebaseapp.com",
-  projectId: "notluyorum-76d68",
-  storageBucket: "notluyorum-76d68.appspot.com",
-  messagingSenderId: "643717030849",
-  appId: "1:643717030849:web:973498216bb2ec2abec540",
-  measurementId: "G-4JBRXM24VT",
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
