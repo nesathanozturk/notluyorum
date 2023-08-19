935abc1b5303f28d12718076ecaa3f5fc8fea8f6
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-08-19 13:48:09 +0300

    feat: add div

--- src/layouts/footer/index.tsx
+++ src/layouts/footer/index.tsx
@@ -3,16 +3,18 @@ import Link from "next/link";
 const Footer = () => {
   return (
     <footer className="footer footer-center py-6 px-4 text-gray-700 dark:text-gray-200 font-semibold text-lg">
-      <p>
-        Copyright © 2023 - Tüm hakları saklıdır.&nbsp;
-        <Link
-          className="text-transparent bg-gradient-to-r from-blue-500 to-purple-600 bg-clip-text"
-          href="https://instagram.com/nesathanozturk/"
-          target="_blank"
-        >
-          Neşathan ÖZTÜRK
-        </Link>
-      </p>
+      <div>
+        <p>
+          Copyright © 2023 - Tüm hakları saklıdır.&nbsp;
+          <Link
+            className="text-transparent bg-gradient-to-r from-blue-500 to-purple-600 bg-clip-text"
+            href="https://instagram.com/nesathanozturk/"
+            target="_blank"
+          >
+            Neşathan ÖZTÜRK
+          </Link>
+        </p>
+      </div>
     </footer>
   );
 };
